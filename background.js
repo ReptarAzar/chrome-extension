29e7cbc57540f50b48a58e72c9417ba20790a35e
Christopher Azar <chris.a@socialcodeinc.com>
2016-03-04 16:00:56 -0800

    go to the socialcode url

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
-    var newURL = "http://socialcode.dev/post/" + postID;
+    var newURL = "http://socialcode.dev/"; // todo: add post/[post-id]
     chrome.tabs.create({ url: newURL });
 });
